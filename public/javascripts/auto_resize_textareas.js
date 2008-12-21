37e19b8631489031ba794b39a8dfe6c21ecc7e18
Micah Geisel <originofstorms@gmail.com>
2008-12-20 19:01:59 -0800

    auto resize textareas

---
+++ public/javascripts/auto_resize_textareas.js
@@ -0,0 +1,70 @@
+$(function() {
+  $("tbody.unbilled td.notes textarea").each(function() {
+    new AutoResizeTextArea.init(this)
+  })
+})
+
+var AutoResizeTextArea = {
+  init: function(el) {
+    // get height of the element  
+    var __heightFromElement = el.offsetHeight;  
+      
+    // adjust the textarea, for all good browsers: overflow:hidden to lose the scrollbars,   
+    // for IE use overflowX:auto to let that browser decide whether to show scrollbars  
+    $(el).css("overflow", "hidden")
+    $(el).css("overflowX", "auto")
+      
+    // create a new element that will be used to track the dimensions  
+    var dummy_id = Math.floor(Math.random()*99999) + '_dummy';  
+    var dummy = document.createElement('div');  
+    document.body.appendChild(dummy);  
+  
+    // hide the created div away  
+    dummy.style.position = 'absolute';  
+    dummy.style.top = '0px';  
+    dummy.style.left = '0px';  
+    dummy.innerHTML = ' 42';  
+      
+    var __lineHeight = dummy.offsetHeight;  
+      
+    var checkExpandContract = function() {  
+      // place text inside the element in a new var called html  
+      $.each(['display', 'font-family', 'font-weight', 'font-size', 'padding', 'margin', 'overflow-x'], function() {
+        $(dummy).css(this, $(el).css(this))
+      })
+      
+      // hack width to fix, 2px too big for some god damned reason
+      var width = $(el).css('width')
+      if(width.charAt(width.length-1) == "x") {  
+        width = parseInt(width.substring(0,width.length-2))   
+      }
+      $(dummy).css('width', width-2+"px")
+
+      // transfer content to dummy      
+      var html = el.value.replace(/\n/g, '<br>');  
+      if(dummy.innerHTML != html) {  
+        dummy.innerHTML = html;  
+          
+        // if the height from our element doesnt match the height from the dummy
+        if (el.offsetHeight != dummy.offsetHeight) {  
+          el.style.height = (dummy.offsetHeight + __lineHeight) + 'px';  
+        }
+      }
+    }
+    
+    // Startup
+    
+    var expandElement = function()  {
+        interval = window.setInterval(function() {checkExpandContract()}, 250);
+    }
+    
+    var contractElement = function() {
+        clearInterval(interval);
+    }
+    
+    // Put eventListeners to our elements
+    $(el).focus(expandElement);
+    $(el).blur(contractElement);
+    checkExpandContract();
+  }
+}
