2df840f7562768a4330f021154a4f4830231f97e
Micah Geisel <originofstorms@gmail.com>
2008-12-21 00:42:13 -0800

    refactored clients controller with resource controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,41 +1,7 @@
-class ClientsController < ApplicationController
-  def index
-  end
+class ClientsController < ResourceController::Base
+  actions :all, :except => :show  
 
-  def new
-    @client = Client.new
-  end
+  create.wants.html { redirect_to clients_path }
+  update.wants.html { redirect_to clients_path }
 
-  def edit
-    @client = Client.find(params[:id])
-  end
-
-  def create
-    @client = Client.new(params[:client])
-
-    if @client.save
-      flash[:notice] = 'Client was successfully created.'
-      redirect_to clients_path
-    else
-      render :action => "new"
-    end
-  end
-
-  def update
-    @client = Client.find(params[:id])
-
-    if @client.update_attributes(params[:client])
-      flash[:notice] = 'Client was successfully updated.'
-      redirect_to clients_path
-    else
-      render :action => "edit" 
-    end
-  end
-
-  def destroy
-    @client = Client.find(params[:id])
-    @client.destroy
-
-    redirect_to clients_path
-  end
 end
