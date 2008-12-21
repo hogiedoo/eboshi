3145e07c18ea624af61a9a133e5876296b7a815c
Micah Geisel <originofstorms@gmail.com>
2008-12-21 00:29:50 -0800

    refactored users controller with resource controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,11 @@
-class UsersController < ApplicationController
-	def index
-		@users = User.all
-	end
-
-  def new
-    @user = User.new
-  end
+class UsersController < ResourceController::Base
+ 
+  actions :all, :except => :show
 
   def create
     cookies.delete :auth_token
-    # protects against session fixation attacks, wreaks havoc with 
-    # request forgery protection.
-    # uncomment at your own risk
-    # reset_session
     @user = User.new(params[:user])
-    @user.save
-    if @user.errors.empty?
+    if @user.save
       self.current_user = @user
       redirect_back_or_default('/')
       flash[:notice] = "Thanks for signing up!"
@@ -23,18 +13,7 @@ class UsersController < ApplicationController
       render :action => 'new'
     end
   end
+  
+  update.wants.html { redirect_to users_path }
 
-  def edit
-  	@user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      flash[:notice] = 'User was successfully updated.'
-      redirect_to edit_user_path(@user) 
-    else
-      render :action => "edit" 
-    end
-  end
 end
