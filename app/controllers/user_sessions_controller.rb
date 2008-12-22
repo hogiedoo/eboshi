84e029b287444bc0f90f08b0373ef778b0b095d5
Micah Geisel <originofstorms@gmail.com>
2008-12-21 17:48:43 -0800

    cleaned up login logout routes and logic

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -11,6 +11,6 @@ class UserSessionsController < ResourceController::Base
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    redirect_to login_path
   end
 end
