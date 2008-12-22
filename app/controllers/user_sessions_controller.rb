a36eddc5d829690843dc25055e995ef55f4d43ec
Micah Geisel <originofstorms@gmail.com>
2008-12-21 17:33:39 -0800

    refactored user sessions controller with resource controller. converted user sessions views to haml.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,19 +1,11 @@
-class UserSessionsController < ApplicationController
+class UserSessionsController < ResourceController::Base
+  actions :new, :create, :destroy
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
 
-  def new
-    @user_session = UserSession.new
-  end
-
-  def create
-    @user_session = UserSession.new(params[:user_session])
-    if @user_session.save
-      flash[:notice] = "Login successful!"
-      redirect_back_or_default '/'
-    else
-      render :action => :new
-    end
+  create do
+    flash "Login successful!"
+    wants.html { redirect_back_or_default '/' }
   end
 
   def destroy
