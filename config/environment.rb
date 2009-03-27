c2ccebcf42d0cf8f71946b7203ae1b62132abb8d
Micah Geisel <originofstorms@gmail.com>
2009-03-26 18:27:02 -0700

    removed old pdf dependencies.

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'pdf-writer', :lib => 'pdf/writer'
   config.gem 'haml'
   config.gem 'resource_controller'
   config.gem 'authlogic'
