e15367f40ba7bed636ce57ed45509a522b90470e
Micah Geisel <originofstorms@gmail.com>
2009-04-10 04:21:32 -0700

    number_to_per_hour should handle nil gracefully.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def number_to_per_hour(value)
+    return nil if value.nil?
     precision = (value % 1 == 0 ? 0 : 2)
     number_to_currency(value, :precision => precision)+"/hr"
   end
