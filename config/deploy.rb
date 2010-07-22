0f41d09a249fad9f65507a53b267a0bb939e33a2
Micah Geisel <micah@botandrose.com>
2010-07-22 02:43:28 -0700

    use bard data asset paths and port 22022 for production.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,11 @@
 set :application, "eboshi"
-role :production, "www@botandrose.com"
-
-namespace :data do
-  namespace :pull do
-    desc "Sync the public/files directory."
-    task :assets, :roles => :production do
-      system "rsync --delete -avz www@botandrose.com:#{application}/public/logos public/"
-      system "rsync --delete -avz www@botandrose.com:#{application}/public/signatures public/"
-    end
-  end
-end
+set :asset_paths, ["public/logos", "public/signatures"]
+role :production, "www@botandrose.com:22022"
 
 desc "Update the crontab file"
 task :update_crontab, :roles => :production do
   run "cd #{application} && whenever --update-crontab #{application}"
 end
 
-after 'data:pull', 'data:pull:assets'
 after "deploy", "update_crontab"
 
