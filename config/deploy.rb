ac61e82166a5dc4e7b3149128524055d62246404
Micah Geisel <micah@botandrose.com>
2010-10-16 11:55:11 -0700

    use bundle exec on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ role :production, "www@botandrose.com:22022"
 
 desc "Update the crontab file"
 task :update_crontab, :roles => :production do
-  run "cd #{application} && whenever --update-crontab #{application}"
+  run "cd #{application} && bundle exec whenever --update-crontab #{application}"
 end
 
 after "deploy", "update_crontab"
